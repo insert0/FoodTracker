2c3656b595db8c0cb70db8a3dab856046b0f4f4d
Dong-Jun Kim <dong-junkim@Dong-Juns-MBP.home>
2016-06-11 16:51:45 -0400

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Dong-Jun Kim on 5/31/16.
+//  Copyright © 2016 Dong-Jun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
